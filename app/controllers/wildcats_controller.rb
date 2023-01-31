c989fd24ea679b2533a71ea74b707c217e0c3b16
bnros <b.ros92@gmail.com>
2023-01-31 14:46:21 -0800

    completed api-endpoints backend

--- app/controllers/wildcats_controller.rb
+++ app/controllers/wildcats_controller.rb
@@ -1,2 +1,19 @@
 class WildcatsController < ApplicationController
+
+    def index
+        wildcats = Wildcat.all
+        render json: wildcats
+      end
+
+      def create
+        # Create a new cat
+        wildcat = Wildcat.create(wildcat_params)
+        render json: wildcat
+      end
+    
+      # Handle strong parameters, so we are secure
+      private
+      def wildcat_params
+        params.require(:wildcat).permit(:name, :age, :enjoys, :dislikes, :image)
+      end
 end
