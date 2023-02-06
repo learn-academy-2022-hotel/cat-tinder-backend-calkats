84d7e5e9d199ad05103338b6a40e94ee7d20ccfc
bnros <b.ros92@gmail.com>
2023-02-06 10:58:12 -0800

    troubleshooting issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_194110) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_30_200338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
